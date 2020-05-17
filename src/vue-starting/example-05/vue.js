0020ea8aecdf8ae35a0162994d237e62d2bc71c3
jcabdom <alu0101051420@ull.edu.es>
2020-05-17 20:13:30 +0100

    New example for using vue with bulma

---
+++ src/vue-starting/example-05/vue.js
@@ -0,0 +1,32 @@
+let app = new Vue({
+  el: '#app',
+  data: {
+    subject: 'Programación de Aplicaciones Interactivas',
+    assingnmentNo: '01',
+    assingment: 'Eight Queens Algorithm',
+    grades: [
+      {
+        name: 'Nico',
+        grade: '8'
+      },
+      {
+        name: 'Fabio',
+        grade: '10'
+      },
+      {
+        name: 'Dani',
+        grade: '9'
+      }
+    ]
+  },
+  methods: {
+    addGrade(grade) {
+      if (grade.grade < 10)
+        grade.grade++;
+    },
+    subGrade(grade) {
+      if(grade.grade > 0)
+        grade.grade--;
+    },
+  },
+})
